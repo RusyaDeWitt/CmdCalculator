53eed4d2c97103070bcf4a1e373974f287420dcd
RusyaDewitt <kashayevruslan@gmail.com>
2021-08-12 15:33:50 +0600

    calc

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            string opr = Console.ReadLine();
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            if (opr == "+")
+                Console.WriteLine(a + b);
+            if (opr == "-")
+                Console.WriteLine(a - b);
+            if (opr == "*")
+                Console.WriteLine(a * b);
+            if (opr == "/")
+                Console.WriteLine(a / b);
+            else
+                Console.WriteLine("error");
+        }
+    }
+}
